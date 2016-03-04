ab42653466bff0cc1ff09de6d3d60c5d2936dce2
lvostinar <lvostinar@laurian>
2016-03-04 14:28:15 +0100

    remove unused

--- ws-jmeter/src/com/servoy/jmeter/ws/WSSampler.java
+++ ws-jmeter/src/com/servoy/jmeter/ws/WSSampler.java
@@ -19,7 +19,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
